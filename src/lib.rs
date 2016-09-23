00cb5ff4cc02162aa5da7363b35e1a3240e0d12c
Denis Kolodin <deniskolodin@gmail.com>
2016-09-23 11:49:05 +0300

    Add doctests and improve names

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,144 @@
+//! This crate contains useful macros for `lua` crate.
+//!
+//! ## Clean up the stack for scope
+//!
+//! Use `auto_cleanup` to revert top of stack to size before scope.
+//!
+//! Usage:
+//!
+//! ```rust
+//! # #[macro_use]
+//! # extern crate lua_macros;
+//! # use lua_macros::lua::State;
+//!
+//! fn main() {
+//!     let mut state = State::new();
+//!
+//!     state.push(1);
+//!
+//!     auto_cleanup!(state, {
+//!         state.push(2);
+//!         state.push(3);
+//!         state.push(4);
+//!         state.push(5);
+//!     });
+//!
+//!     assert_eq!(state.get_top(), 1);
+//! }
+//! ```
+//!
+//! ## Convert arguuments from lua
+//!
+//! Library has macro `convert_arguments` to convert arguments for
+//! any types which implemented `FromLua` trait.
+//!
+//! Usage:
+//!
+//! ```rust
+//! # #[macro_use]
+//! # extern crate lua_macros;
+//! # use lua_macros::lua::{State, Integer, Number};
+//! # use lua_macros::lua::ffi::lua_State;
+//! # use lua_macros::lua::libc::c_int;
+//!
+//! pub unsafe extern "C" fn fun_function(ls: *mut lua_State) -> c_int {
+//!     let mut state = State::from_ptr(ls);
+//!     let (_int, _float, _, _str) = convert_arguments!(state, Integer, Number, _, String)
+//!         .map_err(|pos| {
+//!             let msg = match pos {
+//!                 1 => "integer | integer expected as first argument",
+//!                 2 => "number | float expected as second argument",
+//!                 3 => "any | third argument expected",
+//!                 4 => "string | string expected as fourth argument",
+//!                 _ => "unknown argument for `do` funciton",
+//!             };
+//!             state.arg_error(pos, msg);
+//!         }).unwrap();
+//!     state.push_string("That's OK!");
+//!     1
+//! }
+//!
+//! fn main() {
+//!     let mut state = State::new();
+//!     state.push_fn(Some(fun_function));
+//!     state.set_global("fun");
+//!
+//!     assert!(state.do_string("return fun()").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #1"));
+//!
+//!     assert!(state.do_string("return fun(1)").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #2"));
+//!
+//!     assert!(state.do_string("return fun(1, 2.3)").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #3"));
+//!
+//!     assert!(state.do_string("return fun(1, 2.3, function() end)").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #4"));
+//!
+//!     assert!(!state.do_string("return fun(1, 2.3, {}, \"string\")").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("OK"));
+//!
+//!     assert!(state.do_string("return fun({}, 2.3, {}, \"string\")").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #1"));
+//!
+//!     assert!(state.do_string("return fun(1, 2.3, {}, \"string\", \"extra\")").is_err());
+//!     assert!(state.to_str(-1).unwrap().contains("bad argument #5"));
+//! }
+//! ```
+//!
+//! ## Read HashMap from Lua's table
+//!
+//! Macro `lua_map_table` helps you to unpack strictly typed table.
+//!
+//! Usage:
+//!
+//! ```rust
+//! # #[macro_use]
+//! # extern crate lua_macros;
+//! # use lua_macros::lua::{State, Integer};
+//!
+//! lua_table_type!(UserTable<String, Integer>);
+//!
+//! fn main() {
+//!     let mut state = State::new();
+//!     state.do_string("return {one = 1, two = 2, three = 3}");
+//!     let UserTable(mut table) = state.to_type(-1).unwrap();
+//!     assert_eq!(table.remove("one"), Some(1));
+//!     assert_eq!(table.remove("two"), Some(2));
+//!     assert_eq!(table.remove("three"), Some(3));
+//! }
+//! ```
+//!
+//! ## Adds own userdata
+//!
+//! Macro `lua_userdata` implements userdata features for type.
+//!
+//! Usage:
+//!
+//! ```rust
+//! # #[macro_use]
+//! # extern crate lua_macros;
+//! # use lua_macros::lua::{State, Integer};
+//!
+//! #[derive(Clone)]
+//! enum UserEnum {
+//!   One,
+//!   Two,
+//!   Three,
+//! }
+//!
+//! lua_userdata!(UserEnum);
+//!
+//! fn main() {
+//!     let mut state = State::new();
+//!     UserEnum::attach(&mut state);
+//!
+//!     let ud = UserEnum::One;
+//!     state.push(ud);
+//! }
+//! ```
+
+
 pub extern crate lua;
 
 #[macro_export]
@@ -10,12 +151,9 @@ macro_rules! auto_cleanup {
     }};
 }
 
-/// ```rust
-/// convert_arguments!(state, Number, String);
-/// ```
 #[macro_export]
 macro_rules! convert_arguments {
-    (STRICT $strict:expr, $state:ident, $($from:tt),+) => {{
+    (@strict $strict:expr, $state:ident, $($from:tt),+) => {{
         use $crate::lua::Index;
         let names = [$(stringify!($from),)+];
         let quantity = names.len() as Index;
@@ -32,15 +170,15 @@ macro_rules! convert_arguments {
                 let mut position = 0;
                 let result = ($({
                     position += 1;
-                    convert_arguments!(UNPACK $from, $state, base, position)
+                    convert_arguments!(@unpack $from, $state, base, position)
                 },)+);
                 Ok(result)
             };
             collect()
         })
     }};
-    (UNPACK _, $state:ident, $base:expr, $position:expr) => {()};
-    (UNPACK $from:ty, $state:ident, $base:expr, $position:expr) => {{
+    (@unpack _, $state:ident, $base:expr, $position:expr) => {()};
+    (@unpack $from:ty, $state:ident, $base:expr, $position:expr) => {{
         let opt = $state.to_type::<$from>($base + $position);
         match opt {
             Some(v) => v,
@@ -50,22 +188,24 @@ macro_rules! convert_arguments {
         }
     }};
     ($state:ident, $($from:tt),+) =>
-        (convert_arguments!(STRICT true, $state, $($from),+));
+        (convert_arguments!(@strict true, $state, $($from),+));
 }
 
+/// This macro add wrapper struct, because impossible to implement `FromLua` to `HashMap`
+/// because they are from other crates.
 #[macro_export]
-macro_rules! lua_map_table {
+macro_rules! lua_table_type {
     ($name:ident < $key:ty , $val:ty >) => {
-        struct $name(HashMap<$key, $val>);
+        struct $name(std::collections::HashMap<$key, $val>);
 
-        impl FromLua for $name {
-            fn from_lua(state: &mut State, index: Index) -> Option<Self> {
-                let mut map = HashMap::new();
+        impl $crate::lua::FromLua for $name {
+            fn from_lua(state: &mut $crate::lua::State, index: $crate::lua::Index) -> Option<Self> {
+                let mut map = std::collections::HashMap::new();
                 let index = state.abs_index(index);
                 state.push_nil();
                 while state.next(index) {
                     // Non-strict, because this macro pushes to stack additional values
-                    if let Ok((name, value)) = convert_arguments!(STRICT false, state, $key, $val) {
+                    if let Ok((name, value)) = convert_arguments!(@strict false, state, $key, $val) {
                         map.insert(name, value);
                         state.pop(1); // Pop `key` only
                     } else {
@@ -78,6 +218,8 @@ macro_rules! lua_map_table {
         }
     };
 }
+
+// TODO lua_array_type
 
 #[macro_export]
 macro_rules! lua_userdata {
@@ -100,8 +242,8 @@ macro_rules! lua_userdata {
             }
         }
 
-        impl FromLua for $ud {
-            fn from_lua(state: &mut State, index: Index) -> Option<Self> {
+        impl $crate::lua::FromLua for $ud {
+            fn from_lua(state: &mut $crate::lua::State, index: $crate::lua::Index) -> Option<Self> {
                 unsafe {
                     state.test_userdata_typed::<$ud>(index, $ud::meta_name())
                         .map(|p| p.clone())
@@ -109,18 +251,11 @@ macro_rules! lua_userdata {
             }
         }
 
-        impl ToLua for $ud {
-            fn to_lua(&self, state: &mut State) {
+        impl $crate::lua::ToLua for $ud {
+            fn to_lua(&self, state: &mut $crate::lua::State) {
                 unsafe { *state.new_userdata_typed() = self.clone(); }
                 state.set_metatable_from_registry($ud::meta_name());
             }
         }
     };
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
 }
